1873971c3e7098ef08290b7d59251cfdb81113b7
jeferson.rodrigues <jeferson.rodrigues@cnj.jus.br>
2024-03-16 13:21:49 -0300

    configuracao de upload de artefatos

--- cypress/e2e/comprarProdutos.cy.js
+++ cypress/e2e/comprarProdutos.cy.js
@@ -9,7 +9,7 @@ describe('Login no Sauce Labs', () => {
 
   it('realizar login', () => {
     cy.visit('/');
-    cy.get('[data-test="username"]').type('standard_user');
+    cy.get('[data-test="username"]').type('standard_userX');
     cy.get('[data-test="password"]').type('secret_sauce');
     cy.get('[data-test="login-button"]').click();
   })
